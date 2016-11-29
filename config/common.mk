26ffd9ca403b674c1a543ca3fb1061aa0207dc6d
oecke <bjoern@ptrsn.de>
2016-11-29 16:50:45 +0100

    change .zip file name to tylerdurden build

--- config/common.mk
+++ config/common.mk
@@ -278,7 +278,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 5.7.4
-    CM_VERSION := ResurrectionRemix-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+    CM_VERSION := ResurrectionRemix-M-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-UBER_tyler
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.rr.version=$(CM_VERSION) \
